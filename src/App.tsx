d1c9502e0f9b98f9ef4e718e2bc9b214a22f346b
iTNTPiston <zhaoyizhu2000@gmail.com>
2020-10-10 11:33:25 -0400

    test router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import HomePage from './pages/HomePage';
+import TestPage from './pages/TestPage';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/"  >
+					<HomePage />
+				</Route>
+				<Route exact path="/test"  >
+					<TestPage />
+				</Route>
+				<Route path="*">
+					Not Found page working in progress
+					</Route>
+
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TestPage extends React.Component {
+
+	render() {
+		return (
+			<div>
+				Test page working in progress
+			</div>
+		);
+	};
+};
