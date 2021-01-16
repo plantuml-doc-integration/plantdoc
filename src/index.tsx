05af5e3fe47ee5bd8c542d2ae0564b59989fcc5b
iTNTPiston <zhaoyizhu2000@gmail.com>
2021-01-16 09:16:40 -0500

    update proxy configuration

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,9 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import reduxStore from "./store/reduxStore";
-import axios from 'axios';
+import axios from "axios";
 
-const isLocalhost = Boolean(
-	window.location.hostname === 'localhost' ||
-	// [::1] is the IPv6 localhost address.
-	window.location.hostname === '[::1]' ||
-	// 127.0.0.0/8 are considered localhost for IPv4.
-	window.location.hostname.match(
-		/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-	)
-);
-axios.defaults.baseURL = isLocalhost ? 'http://localhost:5000' : "/api";
+axios.defaults.baseURL = "/api";
 
 ReactDOM.render(
 	<React.StrictMode>
